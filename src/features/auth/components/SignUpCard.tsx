de0c5b15e2e8c2fcabff3f4fe0b5f98f599c23d0
Duy ND <duynd1006@gmail.com>
2024-10-10 23:02:41 +0100

    set up hono & create auth api

--- src/features/auth/components/SignUpCard.tsx
+++ src/features/auth/components/SignUpCard.tsx
@@ -16,17 +16,14 @@ import {
   FormItem,
   FormMessage
 } from "@/components/ui/form"
-
-const formSchema = z.object({
-  firstname: z.string().trim().min(1, "Required"),
-  lastname: z.string().trim().min(1, "Required"),
-  email: z.string().email(),
-  password: z.string().min(8, "Minimum of 8 characters required")
-})
+import { registerSchema } from "../schemas"
+import { useRegister } from "../api/useRegister"
 
 export const SignUpCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const { mutate } = useRegister()
+
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
     defaultValues: {
       firstname: "",
       lastname: "",
@@ -35,9 +32,8 @@ export const SignUpCard = () => {
     }
   })
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-    
+  const onSubmit = (values: z.infer<typeof registerSchema>) => {
+    mutate({ json: values })
   }
 
   return (
---
+++ src/features/auth/server/route.ts
@@ -0,0 +1,31 @@
+import { z } from "zod"
+import { Hono } from "hono"
+import { zValidator } from "@hono/zod-validator"
+import { loginSchema, registerSchema } from "../schemas"
+
+
+const app = new Hono()
+  .post(
+    "/login",
+    zValidator("json", loginSchema),
+    (c) => {
+      const { email, password } = c.req.valid("json")
+
+      console.log({ email, password });
+      
+      return c.json({ email, password })
+    }
+)
+  .post(
+    "/register",
+    zValidator("json", registerSchema),
+    (c) => {
+      const { firstname, lastname, email, password } = c.req.valid("json")
+
+      console.log({ firstname, lastname, email, password });
+
+      return c.json({ firstname, lastname, email, password })
+    }
+  )
+
+export default app
