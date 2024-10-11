bd6e03fccce03ffd848f11d796c99094dfd8a3d4
Duy ND <duynd1006@gmail.com>
2024-10-11 11:11:25 +0100

    implement session middleware

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -6,9 +6,19 @@ import { createAdminClient } from "@/lib/appwrite"
 import { ID } from "node-appwrite"
 import { deleteCookie, setCookie } from "hono/cookie"
 import { AUTH_COOKIE } from "@/features/constants"
+import { sessionMiddleware } from "@/lib/session-middleware"
 
 
 const app = new Hono()
+  .get(
+    "/current",
+    sessionMiddleware,
+    (c) => {
+      const user = c.get("user")
+
+      return c.json({ data: user })
+    }
+  )
   .post(
     "/login",
     zValidator("json", loginSchema),
@@ -64,8 +74,12 @@ const app = new Hono()
   )
   .post(
     "/logout",
-    (c) => {
+    sessionMiddleware,
+    async (c) => {
+      const account = c.get("account")
+
       deleteCookie(c, AUTH_COOKIE)
+      await account.deleteSession("current")
 
       return c.json({ success: true })
     }
