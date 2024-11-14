491fb7112658294fcaf9b0d1b46019eae707caad
Duy ND <duynd1006@gmail.com>
2024-11-14 09:25:11 +0000

    update workspace

---
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -0,0 +1,31 @@
+import { getCurrent } from "@/features/auth/actions";
+import { getWorkspace } from "@/features/workspaces/actions";
+import { EditWorkspaceForm } from "@/features/workspaces/components/EditWorkspaceForm";
+import { redirect } from "next/navigation";
+
+interface WorkspaceIdSettingPageProps {
+  params: {
+    workspaceId: string
+  }
+}
+
+const WorkspaceIdSettingPage = async ({
+  params
+}: WorkspaceIdSettingPageProps) => {
+  const user = await getCurrent()
+  if (!user) redirect("/sign-in")
+  
+  const initialValues = await getWorkspace({ workspaceId: params.workspaceId })
+
+  if (!initialValues) {
+    redirect(`/workspaces/${params.workspaceId}`)
+  }
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <EditWorkspaceForm initialValues={initialValues}/>
+    </div>
+  );
+}
+ 
+export default WorkspaceIdSettingPage;
--- src/features/workspaces/components/EditWorkspaceForm.tsx
+++ src/features/workspaces/components/EditWorkspaceForm.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { createWorkspaceSchema } from "../schemas"
+import { updateWorkspaceSchema } from "../schemas"
 import { z } from "zod"
 import { useRef } from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
@@ -17,36 +17,42 @@ import {
 import { DottedSeparator } from "@/components/DottedSeparator"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { useCreateWorkSpace } from "../api/useCreateWorkspace"
 import Image from "next/image"
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
-import { ImageIcon } from "lucide-react"
+import { ArrowLeftIcon, ImageIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { cn } from "@/lib/utils"
+import { Workspace } from "../types"
+import { useUpdateWorkspace } from "../api/useUpdateWorkspace"
 
-interface CreateWorkSpaceFormProps {
-  onCancel?: () => void
+interface EditWorkSpaceFormProps {
+  onCancel?: () => void;
+  initialValues: Workspace;
 }
 
-export const CreateWorkspaceForm = ({ onCancel }: CreateWorkSpaceFormProps) => {
+export const EditWorkspaceForm = ({ onCancel, initialValues }: EditWorkSpaceFormProps) => {
   const router = useRouter()
-  const { mutate, isPending } = useCreateWorkSpace()
+  const { mutate, isPending } = useUpdateWorkspace()
   const inputRef = useRef<HTMLInputElement>(null)
 
-  const form = useForm<z.infer <typeof createWorkspaceSchema>>({
-    resolver: zodResolver(createWorkspaceSchema),
+  const form = useForm<z.infer <typeof updateWorkspaceSchema>>({
+    resolver: zodResolver(updateWorkspaceSchema),
     defaultValues: {
-      name: ""
+      ...initialValues,
+      image: initialValues.imageUrl ?? ""
     }
   })
 
-  const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
+  const onSubmit = (values: z.infer<typeof updateWorkspaceSchema>) => {
     const finalValues = {
       ...values,
       image: values.image instanceof File ? values.image : ""
     }
     
-    mutate({ form: finalValues }, {
+    mutate({
+      form: finalValues,
+      param: { workspaceId: initialValues.$id }
+    }, {
       onSuccess: ({ data }) => {
         form.reset();
         router.push(`/workspaces/${data.$id}`)
@@ -64,9 +70,17 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkSpaceFormProps) => {
 
   return (
     <Card className="w-full h-full border-none shadow-none">
-      <CardHeader className="flex p-7">
+      <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
+        <Button
+          size="sm"
+          variant="secondary"
+          onClick={onCancel ? onCancel : () => router.push(`/workspaces/${initialValues.$id}`)}
+        >
+          <ArrowLeftIcon className="size-4 mr-2" />
+          Back
+        </Button>
         <CardTitle className="text-xl font-bold">
-          Create a new workspace
+          {initialValues.name}
         </CardTitle>
       </CardHeader>
       <div className="px-7">
@@ -184,7 +198,7 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkSpaceFormProps) => {
                 size="lg"
                 disabled={isPending}
               >
-                Create workspace
+                Save Changes
               </Button>
             </div>
           </form>
