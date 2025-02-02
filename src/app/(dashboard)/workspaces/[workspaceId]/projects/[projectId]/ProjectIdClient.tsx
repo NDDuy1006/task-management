9400ee102b4eaae8a6e3322744a882fbd507227a
Duy ND <duynd1006@gmail.com>
2025-02-02 12:31:24 +0000

    refactor project server actions

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/ProjectIdClient.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { PageError } from "@/components/PageError"
+import { PageLoader } from "@/components/PageLoader"
+import { Button } from "@/components/ui/button"
+import { useGetProject } from "@/features/projects/api/useGetProject"
+import { ProjectAvatar } from "@/features/projects/components/ProjectAvatar"
+import { useProjectId } from "@/features/projects/hooks/useProjectId"
+import { TaskViewSwitcher } from "@/features/tasks/components/TaskViewSwitcher"
+import { PencilIcon } from "lucide-react"
+import Link from "next/link"
+
+export const ProjectIdClient = () => {
+  const projectId = useProjectId()
+  const { data: project, isLoading } = useGetProject({ projectId })
+
+  if (isLoading) {
+    return <PageLoader />
+  }
+
+  if (!project) {
+    return <PageError message="Project Not Found."/>
+  }
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          <ProjectAvatar
+            name={project.name}
+            image={project.imageUrl}
+            className="size-8"
+          />
+          <p className="text-lg font-semibold">{project.name}</p>
+        </div>
+        <div>
+          <Button
+            variant="secondary"
+            size="sm"
+            asChild
+          >
+            <Link
+              href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}
+            >
+              <PencilIcon className="size-4 mr-2"/>
+              Edit Project
+            </Link>
+          </Button>
+        </div>
+      </div>
+      <TaskViewSwitcher hideProjectFilter defaultProjectId={project.$id} />
+    </div>
+  )
+}
--- src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -1,26 +1,13 @@
 import { getCurrent } from "@/features/auth/queries";
-import { EditProjectForm } from "@/features/projects/components/EditProjectForm";
-import { getProject } from "@/features/projects/queries";
 import { redirect } from "next/navigation";
+import { ProjectIdSettingsClient } from "./ProjectIdSettingsClient";
 
-interface ProjectSettingsPageProps {
-  params: {
-    projectId: string;
-  }
-}
-
-const ProjectIdSettingsPage = async ({params}: ProjectSettingsPageProps) => {
+const ProjectIdSettingsPage = async () => {
   const user = await getCurrent()
   if (!user) redirect("/sign-in")
-  
-  const initialValues = await getProject({
-    projectId: params.projectId
-  })
 
   return ( 
-    <div className="w-full lg:max-w-xl">
-      <EditProjectForm initialValues={initialValues}/>
-    </div>
+    <ProjectIdSettingsClient />
   )
 }
  
